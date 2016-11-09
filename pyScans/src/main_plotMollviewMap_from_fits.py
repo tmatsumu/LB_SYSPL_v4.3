aceb7b270dd748ef52cf6f916343741fc7d6e947
MATSUMURA Tomotake <tmatsumu@cw07.cc.kek.jp>
2016-11-09 17:20:27 +0900

    adding a few python codes

---
+++ pyScans/src/main_plotMollviewMap_from_fits.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pylab as py
+import healpy as h
+import sys
+
+dir_in = sys.argv[1]
+filename = sys.argv[2]
+max = float(sys.argv[3])
+min = float(sys.argv[4])
+
+map = h.read_map(dir_in+'/'+filename+'.fits')
+
+max = np.max(map)
+min = 0
+h.mollview(map, max=max, min=min) #, nest=True)
+py.savefig(dir_in+'/'+filename+'.png')
+py.clf()
